c1ab389bcc0e1af2c74e1dd15f75df5e343010ea
Zack Brym <z.t.brym@gmail.com>
2016-07-27 10:13:29 -0500

    Final-assessment-soln.R: fig set 1 & 2

--- code/Final-assessment-soln.R
+++ code/Final-assessment-soln.R
@@ -12,9 +12,39 @@ data <- read.csv("gapminder_all.csv")
 # 1. Line histogram - single variable distribution with many data points
 #        - Most recent population distribution in Asia
 
-Europe <- filter(data, continent=="Europe")
-ggplot(data, aes(pop_2007)) +
-  geom_freqpoly(binwidth=50)
+Africa <- filter(data, continent=="Africa")
+
+# Bad
+ggplot(Africa, aes(log10(pop_2007))) +
+  geom_histogram()
+
+# Good
+ggplot(Africa, aes(log10(pop_2007), ..count..)) +
+  geom_density() +
+  labs(x = "Log10( 2007 Population )", y = "Density") +
+  theme_classic(base_size = 24, base_family = "Helvetica") +
+  theme(axis.title=element_text(size=36))
+
 # 2. Bubble chart - relationship with three variables
+# Bad
+ggplot(data, aes(x = gdpPercap_2007, y = lifeExp_2007, size = pop_2007)) +
+  geom_smooth() +
+  geom_point() +
+  labs(x = "gdpPercap", y = "lifeExp", 
+       size = "Population", title = "2007") +
+  theme_classic()
+
+# Good
+ggplot(data, aes(x = gdpPercap_2007, y = lifeExp_2007, 
+                 size = pop_2007/1000000)) +
+  geom_smooth(method = "lm", color = "black", size = 2) +
+  geom_point() +
+  scale_size(range = c(2, 20)) +
+  scale_x_log10() +
+  labs(x = "GDP/capita [adjusted US$]", y = "Life Expectancy [years]", 
+       size = "Population [millions]", title = "2007") +
+  theme_classic(base_size = 24, base_family = "Helvetica") +
+  theme(axis.title=element_text(size=36))
+
 # 3. Faceted bar chart - comparison among many items/categories
 # 4. Stacked column/area chart - composition changing over time
